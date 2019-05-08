85662180826e765e689194c7eb6f50d168736306
NohemiGlez <ngonzalez.swe@gmail.com>
2019-05-08 08:34:07 -0600

    Add singleton scope to connection bean

--- src/main/java/com/memebrary/util/DatabaseConfig.java
+++ src/main/java/com/memebrary/util/DatabaseConfig.java
@@ -2,19 +2,20 @@ package com.memebrary.util;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @PropertySource({ "classpath:application.properties" })
 public class DatabaseConfig {
 
 	@Bean
-	@Primary
+	@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 	@ConfigurationProperties("spring.datasource")
 	public DataSource dataSource(DataSourceProperties properties) {
 		return properties.initializeDataSourceBuilder().build();
