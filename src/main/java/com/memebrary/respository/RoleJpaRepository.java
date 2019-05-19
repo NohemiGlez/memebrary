e050ae4fe9e2c2a89f372963cce81b7bda8aa88e
Jesus Jose Saenz Fierro <jaredmaster32@gmail.com>
2019-05-19 13:04:39 -0600

    Merge con master

--- src/main/java/com/memebrary/respository/RoleJpaRepository.java
+++ src/main/java/com/memebrary/respository/RoleJpaRepository.java
@@ -2,6 +2,8 @@ package com.memebrary.respository;
 
 import com.memebrary.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleJpaRepository extends JpaRepository<Role, Long> {
 }
--- src/main/java/com/memebrary/respository/UserJpaRepository.java
+++ src/main/java/com/memebrary/respository/UserJpaRepository.java
@@ -2,7 +2,9 @@ package com.memebrary.respository;
 
 import com.memebrary.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserJpaRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
